e790aa4fb6ee51b7f5928dc98a3916f4d9e6e7bc
Johan <johan.svensson@gtsmarketing.se>
2025-03-13 22:34:44 +0100

    divFix

--- src/lib/components/ProductSelection.svelte
+++ src/lib/components/ProductSelection.svelte
@@ -98,7 +98,7 @@
 							<div class="flex h-full items-center justify-center">
 								<div
 									class="h-6 w-6 animate-spin rounded-full border-4 border-primary border-t-transparent"
-								/>
+								></div>
 							</div>
 						</div>
 					{/if}
