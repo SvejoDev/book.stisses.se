4e6ca7911edc8b8749315694af4abc49b94d7e73
Johan <johan.svensson@gtsmarketing.se>
2025-05-15 21:02:50 +0200

    supabase files to ts

--- src/lib/supabaseServerClient.ts
+++ src/lib/supabaseServerClient.ts
@@ -1,3 +1,4 @@
+// import type { Database } from '../../database.types'; // Uncomment when you have generated types
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
@@ -5,4 +6,5 @@ import { SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
 const supabaseUrl = PUBLIC_SUPABASE_URL;
 const supabaseKey = SUPABASE_SERVICE_ROLE_KEY;
 
+// export const supabaseServer = createClient<Database>(supabaseUrl, supabaseKey); // Use when types are ready
 export const supabaseServer = createClient(supabaseUrl, supabaseKey);
